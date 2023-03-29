3b9a64bfa07858ad8d40957c67a504efcc4e1c72
leozizz <leonardocps2015@gmail.com>
2023-03-29 15:42:07 -0300

    Estrutura inicial

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+
+import { useState } from 'react';
+import imagemCachorro from './assets/cachorro.jpg';
+import imagemGato from './assets/gato.jpg';
+
+export default function App() {
+  let imagemAnimal = "";
+  let informacaoAnimal = "";
+  let tipoAnimal = "Cachorro"
+
+  const [tipoDoComponenteCard, setTipoDoCompomenteCard] = useState("cachorro");
+
+  const alterarState = () => {
+    if (tipoDoComponenteCard === "cachorro") {
+      setTipoDoCompomenteCard("gato");
+    } else {
+      setTipoDoCompomenteCard("cachorro");
+    }
+  };
+
+  if (tipoDoComponenteCard === "cachorro") {
+    imagemAnimal = imagemCachorro;
+    informacaoAnimal = "É um mamífero carnívoro da família dos canídeos.";
+    tipoAnimal = "Cachorro";
+  } else {
+    imagemAnimal = imagemGato;
+    informacaoAnimal = "É um mamífero carnívoro da família dos felídeos.";
+    tipoAnimal = "Gato";
+  };
+
+  return (
+    <div className='App'>
+      {/* componente Topo */}
+      <header>
+        <h1>Projeto Props + useState</h1>
+        <p>Clique no botão para mudar os componentes abaixo</p>
+        <button onClick={alterarState}>Mudar Animal</button>
+      </header>
+  
+      {/* Componente CardAnimal */}
+      <div id="CardAnimal">
+        <img src={imagemAnimal} alt="animal" />
+      </div>
+  
+      {/* Componente CardInformacoes */}
+      <div className='CardInformacao'>
+        <h3>Informação sobre o {tipoAnimal}</h3>
+        <p>{informacaoAnimal}</p>
+      </div>
+    </div>
+  );
+}
