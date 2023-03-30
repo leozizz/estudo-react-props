a313ac068f7d93b0c94ecb7727534a30f2f485d6
leozizz <leonardocps2015@gmail.com>
2023-03-30 09:11:49 -0300

    Corrigindo erro no componente CardInformacoes

--- src/componentes/CardInformacoes/index.js
+++ src/componentes/CardInformacoes/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import './index.css'
 
 export default function CardInformacoes(props){
-    <div className='CardInformacao'>
+    return (
+        <div className='CardInformacao'>
         <h3>Informação sobre o {props.tipoAnimal}</h3>
         <p>{props.informacaoAnimal}</p>
-    </div>
+        </div>
+    )
 }
